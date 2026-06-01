688e5c0d784e53bc3512cf5dd0c0a79008d913cc
shiuli.haldar01 <shiuli.haldar01@INHYDL23186>
2026-06-01 20:47:27 +0530

    Add API documentation for Contacts endpoints

--- EarlyCareersPortal.Api/Controllers/ContactsController.cs
+++ EarlyCareersPortal.Api/Controllers/ContactsController.cs
@@ -4,6 +4,9 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace EarlyCareersPortal.Api.Controllers;
 
+/// <summary>
+/// Manages contact information for early career team members and mentors
+/// </summary>
 [ApiController]
 [Route("api/contacts")]
 public class ContactsController : ControllerBase
@@ -12,10 +15,25 @@ public class ContactsController : ControllerBase
 
     public ContactsController(IContactService service) => _service = service;
 
+    /// <summary>
+    /// Retrieves all contacts
+    /// </summary>
+    /// <returns>List of all contacts with name, email, and creation date</returns>
+    /// <response code="200">Returns the list of contacts</response>
     [HttpGet]
+    [ProducesResponseType(typeof(IEnumerable<ContactDto>), StatusCodes.Status200OK)]
     public async Task<IActionResult> GetAll() => Ok(await _service.GetAll());
 
+    /// <summary>
+    /// Adds a new contact to the system
+    /// </summary>
+    /// <param name="dto">Contact details including name and email</param>
+    /// <returns>The newly created contact</returns>
+    /// <response code="200">Contact successfully created</response>
+    /// <response code="400">Invalid contact data provided</response>
     [HttpPost]
+    [ProducesResponseType(typeof(ContactDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<IActionResult> Create([FromBody] CreateContactDto dto)
     {
         var contact = await _service.Create(dto.Name, dto.Email);
@@ -23,4 +41,9 @@ public class ContactsController : ControllerBase
     }
 }
 
+/// <summary>
+/// DTO for creating a new contact
+/// </summary>
+/// <param name="Name">Full name of the contact</param>
+/// <param name="Email">Email address of the contact</param>
 public record CreateContactDto(string Name, string Email);
